f482aad67dfbc4d7afaf508578987734572eddfb
Hedayat Raza <97795268+hrkoo7@users.noreply.github.com>
2024-09-14 23:46:25 +0530

    Update connectDb.js

--- config/connectDb.js
+++ config/connectDb.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDb = async () => {
 	try {
-		await mongoose.connect("mongodb+srv://hedayatrk30:tangent30@cluster0.vi7yzes.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+		await mongoose.connect(process.env.MONGO_URI)
 		console.log(`Database running on ${mongoose.connection.host}`)
 	} catch (error) {
 		console.log(error)
